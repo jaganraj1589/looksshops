a273b284571eb0bb40416cee9e8a3842781c661b
Anu <anupama.p@krds.com>
2021-03-02 19:18:12 +0530

    new theme

---
+++ admin/language/en-gb/extension/module/customlooks.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']     = 'Custom Looksshops Module';
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified Custom Looksshops Module module!';
+$_['text_edit']         = 'Edit Custom Looksshops Module Module';
+// Entry
+$_['entry_name']        = 'Module Name';
+$_['entry_title']       = 'Heading Title';
+$_['entry_status']      = 'Status';
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify Custom Looksshops Module module!';
+$_['error_name']        = 'Module Name must be between 3 and 64 characters!';
