97f42453d63e80efe2a5f9a2f74eaa39f34cd112
Anu <anupama.p@krds.com>
2021-03-05 01:22:00 +0530

    side menu to have all categories

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -1,6 +1,8 @@
 <?php
-class ControllerCommonMenu extends Controller {
-	public function index() {
+class ControllerCommonMenu extends Controller
+{
+	public function index()
+	{
 		$this->load->language('common/menu');
 
 		// Menu
@@ -8,30 +10,32 @@ class ControllerCommonMenu extends Controller {
 
 		$this->load->model('catalog/product');
 
-		$data['categories'] = array();
+		$data['categories'] = [];
+		$data['all_categories'] = [];
 
 		$categories = $this->model_catalog_category->getCategories(0);
 
 		foreach ($categories as $category) {
+			// if ($category['top']) {
+			// Level 2
+			$children_data = array();
+
+			$children = $this->model_catalog_category->getCategories($category['category_id']);
+
+			foreach ($children as $child) {
+				$filter_data = array(
+					'filter_category_id'  => $child['category_id'],
+					'filter_sub_category' => true
+				);
+
+				$children_data[] = array(
+					'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+					'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+				);
+			}
+
+			// Level 1
 			if ($category['top']) {
-				// Level 2
-				$children_data = array();
-
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
-
-				foreach ($children as $child) {
-					$filter_data = array(
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
-					);
-
-					$children_data[] = array(
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
-					);
-				}
-
-				// Level 1
 				$data['categories'][] = array(
 					'name'     => $category['name'],
 					'children' => $children_data,
@@ -39,6 +43,13 @@ class ControllerCommonMenu extends Controller {
 					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
 				);
 			}
+
+			$data['all_categories'][] = array(
+				'name'     => $category['name'],
+				'children' => $children_data,
+				'column'   => $category['column'] ? $category['column'] : 1,
+				'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+			);
 		}
 
 		return $this->load->view('common/menu', $data);
