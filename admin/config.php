0671aa97dc41f3a9273267ed9d6cc8dd25285a06
jaganraj.g <jaganraj.g@krds.fr>
2021-03-08 21:00:04 +0530

    filter and menu fix and blog

--- admin/config.php
+++ admin/config.php
@@ -28,9 +28,9 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'looksshops');
+define('DB_DATABASE', 'lookshop');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+define('DB_PREFIX', '');
 
 // OpenCart API
 define('OPENCART_SERVER', 'https://www.opencart.com/');
--- config.php
+++ config.php
@@ -25,6 +25,6 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'looksshops');
+define('DB_DATABASE', 'lookshop');
 define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+define('DB_PREFIX', '');
